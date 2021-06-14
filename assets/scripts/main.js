868c139d548f689e55d25498b9062581a6623acf
dmobrien <dmobrienllc@gmail.com>
2021-06-14 10:49:14 -0500

    Completed homework; added screenshot.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,14 +2,15 @@
 var generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
-var passwordRules={
+var passwordInfoDto={
   pwdLength:0,
   useLower:false,
   useUpper:false,
   useNumeric:false,
   useSpecialChars:false,
+  password:"",
   writeValues: function(){
-    "Password Length: " +this.pwdLength +
+    return "Password Length: " + this.pwdLength +
     "\nUse Lower Case: " + this.useLower +
     "\nUse Upper Case: " + this.useUpper +
     "\nUse Numeric Chars: " + this.useNumeric +
@@ -18,65 +19,63 @@ var passwordRules={
 }
 
 function writePassword() {
-  var password = generatePassword();
+  var passwordInfoDto = generatePassword();
   var passwordText = document.querySelector("#password");
-  var passwordCritera = document.querySelector("#password-criteria");
+  var passwordCriteria = document.querySelector("#password-criteria");
 
-  passwordText.value = password;
-  passwordCritera.value = passwordRules.writeValues();
+  passwordText.value = passwordInfoDto.password;
+  //passwordCriteria.value = passwordInfoDto.writeValues();
 }
 
-//Called functions
+//Sets up password criteria and returns object
 function generatePassword() {
 
-  passwordRules.pwdLength = getNumberOfCharacters();
-  passwordRules.useLower = confirm("Would you like lowercase characters in your password?");
-  passwordRules.useUpper = confirm("Would you like UPPER CASE characters in your password?");
-  passwordRules.useSpecialChars = confirm("Would you like special characters in your password");
-  passwordRules.useNumeric = confirm("Would you like numeric characters in your password");
-  console.log(passwordRules.writeValues());
+  passwordInfoDto.pwdLength = validateNumberOfChars();
+  passwordInfoDto.useLower = confirm("Would you like lowercase characters in your password?");
+  passwordInfoDto.useUpper = confirm("Would you like UPPER CASE characters in your password?");
+  passwordInfoDto.useSpecialChars = confirm("Would you like special characters in your password");
+  passwordInfoDto.useNumeric = confirm("Would you like numeric characters in your password");
 
   //init pwd char vars
-  var length = (passwordRules.pwdLength>0)?(passwordRules.pwdLength):(8);
+  var length = (passwordInfoDto.pwdLength>0)?(passwordInfoDto.pwdLength):(8);
   var alphaChars = "abcdefghijklmnopqrstuvwxyz"; //to upper 
   var numericChars = '0123456789';
   var specialChars = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
-  var password = "";
   var character = "";
 
-    while( password.length<length ) {
-      if(passwordRules.useLower)
+    while( passwordInfoDto.password.length<length ) {
+      if(passwordInfoDto.useLower)
       {
         alphaCharSelector = Math.ceil(alphaChars.length * Math.random()*Math.random());
         character += alphaChars.charAt( alphaCharSelector );
       }
 
-      if(passwordRules.useUpper)
+      if(passwordInfoDto.useUpper)
       {
         alphaCharSelector = Math.ceil(alphaChars.length * Math.random()*Math.random());
         character += alphaChars.charAt( alphaCharSelector ).toUpperCase();
       }
         
-      if(passwordRules.useNumeric)
+      if(passwordInfoDto.useNumeric)
       {
         numCharSelector = Math.ceil(numericChars.length * Math.random()*Math.random());
         character += numericChars.charAt( numCharSelector );
       }
         
-      if(passwordRules.useSpecialChars)
+      if(passwordInfoDto.useSpecialChars)
       {
         specCharSelector = Math.ceil(specialChars.length * Math.random()*Math.random());
         character += specialChars.charAt( specCharSelector );
       }
       
-      password = character;
+      passwordInfoDto.password = character;
     }
-    password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
-    return password.substr(0,passwordRules.pwdLength);
+    passwordInfoDto.password=passwordInfoDto.password.split('').sort(function(){return 0.5-Math.random()}).join('');
+    passwordInfoDto.password.substr(0,passwordInfoDto.pwdLength);
+    return passwordInfoDto;
 }
 
-
-function getNumberOfCharacters(){
+function validateNumberOfChars(){
   let input = 0;
   while (input = prompt("Please enter desired password length, between 8 and 128 characters!",8)) {
     if (isNaN(input) || 
@@ -91,27 +90,3 @@ function getNumberOfCharacters(){
   return 8;
 }
 
-
-
-// function generatePassword( len ) {
-//   var length = (len)?(len):(10);
-//   var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
-//   var numeric = '0123456789';
-//   var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
-//   var password = "";
-//   var character = "";
-//   var crunch = true;
-//   while( password.length<length ) {
-//       entity1 = Math.ceil(string.length * Math.random()*Math.random());
-//       entity2 = Math.ceil(numeric.length * Math.random()*Math.random());
-//       entity3 = Math.ceil(punctuation.length * Math.random()*Math.random());
-//       hold = string.charAt( entity1 );
-//       hold = (password.length%2==0)?(hold.toUpperCase()):(hold);
-//       character += hold;
-//       character += numeric.charAt( entity2 );
-//       character += punctuation.charAt( entity3 );
-//       password = character;
-//   }
-//   password=password.split('').sort(function(){return 0.5-Math.random()}).join('');
-//   return password.substr(0,len);
-// }
